3428c63483e1c5f9ef312dbbf46fd86031238240
hong235 <67294935+hong235@users.noreply.github.com>
2020-06-26 16:45:42 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
-import sys
+import os, sys
 import subprocess
 
-cmd = ['report.py --config report.ini']
-subprocess.Popen([cmd])
+#cmd = ['report.py --config report.ini']
 #subprocess.Popen([sys.executable, cmd], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+os.system('python report.py --config report.ini')
