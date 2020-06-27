89f7a677dd6418ef3be0d935008b59b7e4b7678e
hong235 <67294935+hong235@users.noreply.github.com>
2020-06-26 17:17:53 -0700

    Update app.py

--- app.py
+++ app.py
@@ -2,4 +2,4 @@ import os, sys
 import subprocess
 
 #os.system('python report.py --config report.ini')
-os.system('python /opt/app-root/src/report.py --config /opt/app-root/src/report.ini')
+os.system('python /opt/app-root/src/report.py --config /opt/app-root/src/report.ini') #same output as above
